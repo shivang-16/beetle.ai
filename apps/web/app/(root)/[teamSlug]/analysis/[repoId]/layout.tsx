362405445ae9204eaad2770f4b98a641962c6fc2
shivang-16 <shivangyadav121@gmail.com>
2025-10-07 15:15:50 +0530

    refactor(analysis): pass analysisList as prop to sidebar component

--- apps/web/app/(root)/[teamSlug]/analysis/[repoId]/layout.tsx
+++ apps/web/app/(root)/[teamSlug]/analysis/[repoId]/layout.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import AnalysisSidebar from "../../../analysis/[repoId]/_components/analysis-sidebar";
 import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable";
+import { getAnalysisWithId } from "@/app/(root)/analysis/[repoId]/_actions/getAnalysiswithId";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -16,15 +17,19 @@ export default async function TeamAnalysisLayout({
 }: LayoutProps) {
   const resolvedParams = await params;
   const { teamSlug, repoId } = resolvedParams;
+  const analysisList = await getAnalysisWithId(repoId);
 
   return (
     <div className="h-screen">
       <Suspense>
         <ResizablePanelGroup direction="horizontal" className="h-full">
-          <ResizablePanel defaultSize={20} minSize={4} maxSize={36}>
-            <AnalysisSidebar repoId={repoId} />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
+           {analysisList && analysisList.length > 0 && (
+              <>
+                <ResizablePanel defaultSize={20} minSize={4} maxSize={25}>
+                  <AnalysisSidebar repoId={repoId} analysisList={analysisList} />
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+              </>)}
           <ResizablePanel defaultSize={75}>
             <div className="h-full">{children}</div>
           </ResizablePanel>
